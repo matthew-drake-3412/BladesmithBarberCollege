0360925beea171bd389f99cfcfacb53d9cc22094
matthew-drake-3412 <matthewdrake@my.unt.edu>
2026-09-25 02:08:01 +0000

    Updated start page and upload page

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -5,20 +5,57 @@ class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     return Scaffold(
-      appBar: AppBar(title: const Text('Start')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => UploadPage()),
-            );
-          },
-          child: const Text('Upload a photo'),
-        ),
-      ),
+      body: Container(
+        color: Color.fromARGB(255, 32, 30, 30),
+        child: Center(child: 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container( margin: const EdgeInsets.all(24), child: 
+              Text(
+                'Gradients AI',
+                style: TextStyle(
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              )),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UploadPage()),
+                  );
+                },
+                child: const Text('Start', style: TextStyle(fontSize: 24, color: Colors.black)),
+              )
+          ],
+        )),
+      )
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: const Text('')),
+  //     body: Center(
+  //       child: ElevatedButton(
+  //         onPressed: () {
+  //           Navigator.push(
+  //             context,
+  //             MaterialPageRoute(builder: (context) => UploadPage()),
+  //           );
+  //         },
+  //         child: const Text('Upload a photo'),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  
 }
--- lib/pages/upload_page.dart
+++ lib/pages/upload_page.dart
@@ -12,22 +12,56 @@ class UploadPage extends StatefulWidget {
 class _UploadPageState extends State<UploadPage> {
   List<XFile> photos = []; // starts empty
 
+  // Put next page logic here
+  void nextPage() 
+  {
+    // Navigate to the next page, passing the selected photos
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //     builder: (context) => NextPage(photos: photos),
+    //   ),
+    // );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Upload Photo')),
+      appBar: AppBar(title: const Text('Upload Photo'), centerTitle: true),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ImageSelectorWidget(
+            
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: Column(children: [
+            
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: ImageSelectorWidget(
               onPhotosSelected: (selected) {
                 setState(() => photos = selected);
               },
             ),
-            const SizedBox(height: 20),
+            ),
+            // const SizedBox(height: 20),
             Text('${photos.length} photos selected'),
             for (final photo in photos) Text(photo.name),
+
+            ]),
+            ),
+
+            ElevatedButton(
+              onPressed: photos.isNotEmpty ? nextPage : null,
+              child: const Text("Next")
+            )
           ],
         ),
       ),
